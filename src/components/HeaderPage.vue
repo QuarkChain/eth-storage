420c1a8f1aad17353946860e4824c5e17d3acbd3
iteyelmp@gmail.com <lmp19890127>
2022-09-16 10:33:31 +0800

    update ui

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
-    <div class="header-bg">
-      <div class="header">
-        <img class="logo" src="@/assets/logo.png" alt="logo">
-        <div class="header-content">
-          <div>
-            <p class="header-title1">Scaling Storage</p>
-            <p class="header-title2">of The World Computer</p>
-          </div>
-          <img class="header-logo" src="@/assets/header-logo.png" alt="header logo">
+    <div class="logo">
+      <img class="logo-icon" src="@/assets/logo.png" alt="logo">
+    </div>
+    <div class="header">
+      <div class="header-content">
+        <div>
+          <p class="header-title1">Scaling Storage</p>
+          <p class="header-title2">of The World Computer</p>
         </div>
+        <img class="header-logo" src="@/assets/header-logo.png" alt="header logo">
       </div>
     </div>
   </div>
@@ -19,55 +19,51 @@
 .home {
   background: linear-gradient(91deg, #DAE3EB 0%, #EEF2FF 100%);
 }
-.header-bg {
-  background: url("../assets/header-bg.png") no-repeat center;
-  background-size: 1450px;
-}
-.header {
-  max-width: 1200px;
-  display: flex;
-  flex-direction: column;
-  margin: 0 auto;
-}
 
 .logo {
-  margin: 20px auto 0 0;
-  height: 80px;
+  max-width: 1340px;
   display: flex;
   flex-direction: row;
-  justify-content: left;
+  margin: 0 auto;
+}
+.logo-icon {
+  margin: 20px -30px 0;
+  height: 100px;
+}
+
+.header {
+  background: url("../assets/header-bg.png") no-repeat center top;
+  background-size: auto 95%;
 }
 
 .header-content {
   display: flex;
   flex-direction: row;
   justify-content: center;
-  align-items: flex-start;
-  margin-top: 80px;
-  margin-bottom: 80px;
+  align-items: center;
+  padding-top: 80px;
+  padding-bottom: 80px;
 }
 .header-title1 {
-  height: 80px;
-  font-size: 75px;
+  font-size: 90px;
+  line-height: 90px;
   font-weight: normal;
   color: #000000;
-  line-height: 75px;
-  margin-top: 120px;
+  margin-top: -60px;
   font-family: AlibabaPuHuiTiH;
 }
 .header-title2 {
   background: #000000;
   border-radius: 60px;
-  margin-top: 30px;
-  font-size: 45px;
-  line-height: 45px;
-  padding: 15px;
   font-weight: normal;
   color: #FFFFFF;
+  margin-top: 40px;
+  font-size: 50px;
+  line-height: 50px;
+  padding: 20px 50px;
   font-family: AlibabaPuHuiTiH;
 }
 .header-logo {
-  width: 700px;
-  margin-left: -150px;
+  width: 650px;
 }
 </style>
