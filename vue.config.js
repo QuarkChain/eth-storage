785ee033da447dfb68128820d5e72fe29955f50b
iteye <iteyelmp@gmail.com>
2024-09-11 16:47:28 +0800

    assets optimization

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,19 @@
+const webpack = require('webpack');
+
 module.exports = {
   configureWebpack:{
     optimization: {
       splitChunks: {
+        chunks: 'all',
         minSize: 1024000,
         maxSize: 1024000,
       }
-    }
+    },
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 3,
+      })
+    ]
   },
   productionSourceMap: false,
   publicPath: '/',
